eb87c4a7b00ca6b28b160d0a1042c2af85480054
Andre R. | Wires <30797601+AndreRl@users.noreply.github.com>
2018-06-02 00:43:55 +0100

    Language files
    Implemented language files for default language (English)

---
+++ inc/languages/english/admin/testaccount.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Test Account Plugin
+ * Official Language File
+ * 
+ *
+ * Author: Wires (AndreRl)
+ * Website: https://oseax.com
+*/
+
+// Settings Group
+$l['testaccount_desc'] = 'Allows forum administrators to give out a test account with very limited permissions.';
+$l['testaccount_descnote'] = 'Note: Settings configured here may override usergroup permissions.';
+$l['testaccount_installnote'] = 'You currently have an account called \'Test\'. Installing & Activating this plugin will remove this account.';
+// Settings
+$l['testaccount_settinggroupdesc'] = 'Toggle Test Account features. Note: Settings configured here may override usergroup settings.';
+$l['testaccount_enable'] = 'Enable Plugin';
+$l['testaccount_enabledesc'] = 'Enable Test Account Plugin along with its features.';
+$l['testaccount_unlock'] = 'Unlock Test Account';
+$l['testaccount_unlockdesc'] = 'Can the Test account be logged into?';
+$l['testaccount_posting'] = 'Post Restriction';
+$l['testaccount_postingdesc'] = 'Can Test account post?';
+$l['testaccount_sigavatar'] = 'Signature & Avatar Restriction';
+$l['testaccount_sigavatardesc'] = 'Can Test account use Signatures and Avatars?';
+$l['testaccount_pms'] = 'Toggle PMs';
+$l['testaccount_pmsdesc'] = 'Can Test account use Private Message system?';
+$l['testaccount_reputation'] = 'Toggle Reputation';
+$l['testaccount_reputationdesc'] = 'Can Test account use Reputation system?';
+$l['testaccount_statistics'] = 'Enable Statistics';
+$l['testaccount_statisticsdesc'] = 'Enable the statistics and debugging information.';
+
+// Misc
+$l['testaccount_adminerror'] = 'Please change Test account\'s password.';
---
+++ inc/languages/english/testaccount.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test Account Plugin
+ * Official Language File
+ * 
+ *
+ * Author: Wires (AndreRl)
+ * Website: https://oseax.com
+*/
+
+// Template
+
+$l['testaccount_title'] = 'Test Account Statistics';
+$l['testaccount_thead'] = 'Statistics';
+$l['testaccount_mybbv'] = 'MyBB Version';
+$l['testaccount_phpv'] = 'PHP Version';
+$l['testaccount_database'] = 'Database';
+$l['testaccount_debuginfo'] = 'Debugging Information';
+
+$l['testaccount_emailpass'] = 'You are not able to change your Email or Password.';
+$l['testaccount_logincheck'] = 'Username not allowed.';
+$l['testaccount_permissionerror'] = 'You are not able to perform this action.';
