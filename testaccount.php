78fdacd0fb2cc3cd87be391ae492eb905fba6bac
Andre R. | Wires <30797601+AndreRl@users.noreply.github.com>
2018-06-01 01:08:24 +0100

    Add files via upload

---
+++ testaccount.php
@@ -0,0 +1,40 @@
+<?php 
+
+define('IN_MYBB', 1); require "./global.php";
+
+if($mybb->get_input('action') != 'statistics')
+{
+	return;
+}
+	
+add_breadcrumb("Test Account - Statistics", "testaccount.php"); 
+
+// Lets check who we're dealing with
+
+$user = get_user($mybb->user['uid']);
+if($user['username'] != 'Test')
+{
+	error_no_permission();
+} else if($mybb->settings['testaccount_enable'] != 1)
+{
+	error_no_permission();
+} else if($mybb->settings['testaccount_unlock'] != 1)
+{
+	error_no_permission();
+}
+
+// Lets define some variables
+
+$mybbv = $mybb->version;
+$phpv = phpversion();
+
+$databaseengine = $db->short_title;
+$dbi = $db->get_version();
+
+ $database = $databaseengine .' ' . $dbi;
+
+eval("\$html = \"".$templates->get("testaccount_statistics")."\";"); 
+
+output_page($html);
+
+?>
